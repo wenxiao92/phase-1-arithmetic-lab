5eb2f5e6c2ccc8667c8ea2c1effbd037c1045e32
Wen Xiao <wennstyle@gmail.com>
2021-09-09 17:04:39 -0400

    commit tree 0b3b782b76a28e1da108371620ef69e1ead10035

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+// equation to equal 62 for the variable multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2); //does not require parenthesis
+multiply;
+
+//generates a random integer greater than 0
+
+let random = Math.ceil(Math.random() * 10);
+
+//is an equation that calulates a remainder that is equal to 4
+let num3 = 104;
+let num4 = 5;
+let mod = num3 % num4; 
+mod;
+
+//will return 20 as the highest number in the set
+let max = Math.max(5, 10, 15, 20);
